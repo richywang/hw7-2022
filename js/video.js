8b49e81e83872ae69af70b5d248589d50258d764
richywang <riwa@umich.edu>
2022-11-02 17:01:03 -0400

    Finished JS

--- js/video.js
+++ js/video.js
@@ -8,6 +8,7 @@ window.addEventListener("load", function() {
 	video.autoplay = false;
 	video.loop = false;
 
+	this.document.querySelector("#volume").textContent = (video.volume * 100) + "%";
 });
 
 document.querySelector("#play").addEventListener("click", function() {
@@ -15,4 +16,47 @@ document.querySelector("#play").addEventListener("click", function() {
 	video.play();
 });
 
+document.querySelector("#pause").addEventListener("click", function() {
+	video.pause();
+});
 
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 0.9;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate / 0.9;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	const time = video.currentTime + 10;
+	video.currentTime = time >= video.duration ? 0 : time;
+	console.log(video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if(video.muted) {
+		video.muted = false;
+		document.querySelector("#mute").textContent = "Mute"
+	} else {
+		video.muted = true;
+		document.querySelector("#mute").textContent = "Unmute"
+	}
+	
+});
+
+document.querySelector("#slider").addEventListener("change", function() {
+	const volume = document.querySelector("#slider").value;
+	document.querySelector("#volume").textContent = volume + "%";
+	video.volume = volume / 100;
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+})

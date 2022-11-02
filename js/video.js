8c6fd36f0fcc86551fb044829bb32f827a219a54
richywang <riwa@umich.edu>
2022-11-02 17:21:08 -0400

    Fix play button volume

--- js/video.js
+++ js/video.js
@@ -12,7 +12,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
-	this.document.querySelector("#volume").textContent = (video.volume * 100) + "%";
+	document.querySelector("#volume").textContent = (video.volume * 100) + "%";
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
